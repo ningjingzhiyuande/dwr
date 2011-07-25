93221865e2ea42a5d5793ed6928257e8ad3e0f37
oreatial <oreatial@gmail.com>
2011-07-25 20:16:55 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwr::Application.config.secret_token = '1c61cd283a71a1f1f1ceddfdc44d805da0cb587771b00266d70fd6a342ce16cd8603436182e38a031e6307e52a2e03ff88fb4f0b1b5bac1a4acce89b8e730676'
